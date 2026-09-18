4c8fee988374efa5760f0dc6e7c505f54fdd291f
yorche3 <hyaoki123@gmail.com>
2026-09-18 06:10:16 -0600

    chore(algorithms): add scaffold and tests for naive sort

---
+++ core/algorithms/naive_sort/src/NaiveSort.hx
@@ -0,0 +1,15 @@
+package src;
+
+class NaiveSort {
+    public static function selectionSort(arr:Array<Int>):Array<Int> {
+        return null;
+    }
+
+    public static function bubbleSort(arr:Array<Int>):Array<Int> {
+        return null;
+    }
+
+    public static function insertionSort(arr:Array<Int>):Array<Int> {
+        return null;
+    }
+}
